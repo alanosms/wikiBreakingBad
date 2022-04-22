770bcd5747f18bde73ab798a7bc461f89b6dfa18
Alan S. Martins <alan-martins07@hotmail.com>
2022-04-22 13:49:46 -0300

    fix: pagination

--- src/controlsPage.js
+++ src/controlsPage.js
@@ -12,28 +12,40 @@ prev.addEventListener('click', prevPage);
     var offset = 0;
 
 function nextPage(){
-    if (numberPage.innerHTML < 6){
+    if (numberPage.innerHTML < 7){
     numberPage.innerHTML++;
     offset = offset + 10;
-    createTable('https://breakingbadapi.com/api/characters?limit=10&offset='+offset);
+    clearTable();
+   createTable('https://breakingbadapi.com/api/characters?limit=10&offset='+offset);
     }
 }
 function goToFirst(){
-    alert('COOMING SOON');
     numberPage.innerHTML = 1;
     offset = 0;
     var link = 'https://breakingbadapi.com/api/characters?limit=10&offset='+offset;
+    clearTable();
+    createTable(link);
 }
 function goToLast(){
-    alert('COOMING SOON');
     offset = 60;
-    numberPage.innerHTML = 6;
-    var link = 'https://breakingbadapi.com/api/characters?limit=15&offset='+offset;
+    numberPage.innerHTML = 7;
+    var link = 'https://breakingbadapi.com/api/characters?limit=10&offset='+offset;
+    clearTable();
+    createTable(link);
 }
 function prevPage(){
-    alert('COOMING SOON');
     if (numberPage.innerHTML > 1){
         numberPage.innerHTML--;
         offset = offset - 10;
+        var link = 'https://breakingbadapi.com/api/characters?limit=10&offset='+offset;
+        clearTable();
+        createTable(link);
         }
 }
+function clearTable() {
+    characters.forEach(element => { 
+    document.getElementById("table").deleteRow(1);
+});  
+}
+  
+
