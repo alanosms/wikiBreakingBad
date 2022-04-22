1783f7719c1b2e063cd56e805adb610848688fee
Alan S. Martins <alan-martins07@hotmail.com>
2022-04-22 13:48:53 -0300

    fix: changed for to forEach

--- src/apiCharacter.js
+++ src/apiCharacter.js
@@ -1,12 +1,4 @@
 let tbody = document.getElementById("tbody");
-let td_id,
-  td_name,
-  td_birthday,
-  td_nickname,
-  td_status,
-  td_portrayed,
-  td_img,
-  img;
 
 createTable("https://breakingbadapi.com/api/characters?limit=10&offset=0");
 
@@ -19,25 +11,26 @@ function requestApi(url) {
 
 function createTable(url) {
   characters = requestApi(url);
-  for (var c in characters) {
+  characters.forEach(element => {
     let tr = tbody.insertRow();
-    td_id = tr.insertCell();
-    td_name = tr.insertCell();
-    td_birthday = tr.insertCell();
-    td_nickname = tr.insertCell();
-    td_status = tr.insertCell();
-    td_portrayed = tr.insertCell();
-    td_img = tr.insertCell();
+    let td_id = tr.insertCell();
+    let td_name = tr.insertCell();
+    let td_birthday = tr.insertCell();
+    let td_nickname = tr.insertCell();
+    let td_status = tr.insertCell();
+    let td_portrayed = tr.insertCell();
+    let td_img = tr.insertCell();
     img = document.createElement("img");
 
-    img.src = characters[c].img;
-    td_id.innerHTML = characters[c].char_id;
-    td_name.innerHTML = characters[c].name;
-    td_birthday.innerHTML = characters[c].birthday;
-    td_nickname.innerHTML = characters[c].nickname;
-    td_status.innerHTML = characters[c].status;
-    td_portrayed.innerHTML = characters[c].portrayed;
+    img.src = element.img;
+    td_id.innerHTML = element.char_id;
+    td_name.innerHTML = element.name;
+    td_birthday.innerHTML = element.birthday;
+    td_nickname.innerHTML = element.nickname;
+    td_status.innerHTML = element.status;
+    td_portrayed.innerHTML = element.portrayed;
     td_img.appendChild(img);
-  }
+  });
+
 }
 
